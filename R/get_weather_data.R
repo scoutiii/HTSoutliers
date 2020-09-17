e62c4272c0dfa50f1ec9e196362841db50abfc5a
scoutiii <33967844+scoutiii@users.noreply.github.com>
2020-09-16 21:16:25 -0600

    Updating documentation

--- R/get_weather_data.R
+++ R/get_weather_data.R
@@ -1,3 +1,20 @@
+#' Get Weather Data From the GHCN-D
+#'
+#' This is a wrapper function for the 3 main get_x_data functions in snowload2
+#'
+#' You can enter in a list containing states, station IDS, and eco regions
+#' all at once, and get the weather data from all of them in one data frame.
+#'
+#'
+#' @param locations character
+#' @param path character
+#' @param ... other arguments
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#' # data <- get_weather_data(C("UT", "BF1BI000001", "8.2.4"), "D:/Data/ghcnd_all/")
 get_weather_data <- function(locations,
                              path,
                              ...) {
